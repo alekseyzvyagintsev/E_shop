0b7bdc0bf77a3e85aa470a4064b6ae3aa0d71bab
alekseyzvyagintsev <alex0236889@gmail.com>
2025-05-04 15:05:18 +0300

    дописаны фикстуры для тестирования нового функционала

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,9 @@ import pytest
 
 from src.models.category import Category
 from src.models.iterator import ProductIterator
+from src.models.lawngrass import LawnGrass
 from src.models.product import Product
+from src.models.smartphone import Smartphone
 
 
 # Фикстуры для инициализации объектов
@@ -33,6 +35,32 @@ def second_product() -> "Product":
 def third_product() -> "Product":
     return Product("Xiaomi Redmi Note 11", "1024GB, Синий", 30000.0, 10)
 
+
+@pytest.fixture
+def smartphone():
+    return Smartphone(
+        "Samsung Galaxy S23 Ultra",
+        "256GB, Серый цвет, 200MP камера",
+        180000.0,
+        5,
+        95.5,
+        "S23 Ultra",
+        256,
+        "Серый"
+    )
+
+
+@pytest.fixture
+def lawngrass():
+    return LawnGrass(
+        "Газонная трава",
+        "Элитная трава для газона",
+        500.0,
+        20,
+        "Россия",
+        "7 дней",
+        "Зеленый"
+    )
 
 @pytest.fixture
 def fake_product() -> dict[str, Any]:
