b687aa4ad60e8ceabbe5e8d3cca70d1fa8669be6
Aleksey Zvyagintsev <alex0236889@gmail.com>
2025-05-07 22:33:30 +0300

    добавлено описание в base_product.py

--- src/models/base_product.py
+++ src/models/base_product.py
@@ -3,8 +3,10 @@ from typing import Any
 
 
 class BaseProduct(ABC):
+    """ абстрактный класс для создаваемых продуктов """
 
     @classmethod
     @abstractmethod
     def new_product(cls, *args: Any, **kwargs: Any) -> "BaseProduct":
+        """ Метод для добавления нового продукта """
         pass
